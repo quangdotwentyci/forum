1003f5a4f5ac1799aaa4b9252ef030709be4b05b
Quang Do <quang.do@twentyci.asia>
2019-09-19 16:18:24 +0700

    init db'

--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -2,16 +2,20 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Thread;
+use App\Reply;
 use App\User;
+use App\Thread;
+
 use Faker\Generator as Faker;
 
-$factory->define(Thread::class, function (Faker $faker) {
+$factory->define(Reply::class, function (Faker $faker) {
     return [
         'user_id' => function() {
             return factory(User::class)->create()->id;
         },
-        'title' => $faker->sentence,
+        'thread_id' => function() {
+            return factory(Thread::class)->create()->id;
+        },
         'body' => $faker->paragraph
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Reply;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +13,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(\App\Thread::class, 10)->create()->each(function ($thread) {
+            factory(Reply::class, 10)->create([
+                'thread_id' => $thread->id,
+                'user_id' => $thread->user_id,
+            ]);
+        });
     }
 }
