c40363d1bb309d56b306f24505f85342c8ebdade
Quang Do <quang.do@twentyci.asia>
2019-09-24 17:18:50 +0700

    extracting to view component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,12 +35,13 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
                         <li><a href="/threads">All Threads</a></li>
+                        <li><a href="/threads/create">Create thread</a></li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
                                aria-expanded="false">Channels <span class="caret"></span></a>
 
                             <ul class="dropdown-menu">
-                                @foreach (App\Channel::all() as $channel)
+                                @foreach ($channels as $channel)
                                     <li><a href="/threads/{{ $channel->slug }}">{{ $channel->name }}</a></li>
                                 @endforeach
                             </ul>
