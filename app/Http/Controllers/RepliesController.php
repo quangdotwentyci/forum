2e6c960c8cce7e3e005be39dd22e2b6148140d5b
Quang Do <quang.do@twentyci.asia>
2019-09-24 13:37:56 +0700

    add channel

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -19,10 +19,11 @@ class RepliesController extends Controller
     /**
      * Persist a new reply.
      *
-     * @param  Thread $thread
+     * @param  integer $channelId
+     * @param  Thread  $thread
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Thread $thread)
+    public function store($channelId, Thread $thread)
     {
         $thread->addReply([
             'body' => request('body'),
